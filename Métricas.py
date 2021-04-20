52eafccec560c15746eec0298b13277918b9c7ea
pandeX <henricao_7@yahoo.com.br>
2021-04-20 12:52:28 -0300

    testando

---
+++ Métricas.py
@@ -0,0 +1,11 @@
+from igraph import Graph
+from igraph import plot
+import igraph
+
+# Carregamento de grafo no formato graphml
+grafo = igraph.load("C:/Users/Cliente/Desktop/Python/Grafos/Grafo.graphml")
+
+C:/Users/Cliente/Desktop/Python/Grafos/Grafo.graphml
+
+
+grafo = igraph.load('C:/Users/Cliente/Desktop/Python/Grafos/Grafo.graphml')

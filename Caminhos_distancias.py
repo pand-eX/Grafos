a5e916f5cb9c766da852ee174c262177047ae946
pandeX <henricao_7@yahoo.com.br>
2021-04-21 21:46:19 -0300

    distancias grafos

--- Caminhos_distancias.py
+++ Caminhos_distancias.py
@@ -2,5 +2,3 @@ from igraph import Graph
 from igraph import plot
 
 # Criação de grafo direcionado com pesos entre as arestas
-grafo = Graph(edges = [(0,2),(0,1),(1,4),(1,5),(2,3),(6,7),(3,7),(4,7),(5,6)],
-                       directed = True)
